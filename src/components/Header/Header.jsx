f621de608f943baa2c7c9af45def66b20533f7f8
Artem Erusov <erusovav@gmail.com>
2025-01-21 19:18:52 +0300

    Made a hat

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import { formatDate } from '../../helpers/formatDate';
+import styles from './styles.module.css';
+
+function Header() {
+    return (
+    <>
+      <header className={`${styles.header} ${styles.container}`}>
+        <h1 className={styles.title}>News Reacty</h1>
+        <p className={styles.date}>{formatDate(new Date)}</p>
+      </header>
+      <div className={styles.divider}></div>
+    </>
+    )
+  }
+  
+  export default Header;
